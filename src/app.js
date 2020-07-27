0e90db533f9c9ce86ad45454a7471494cded4c26
zhu <zhu_huang@126.com>
2020-07-28 02:52:18 +0800

    将svg代码整理到icon.vue

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import Button from './button'
+import Icon from './icon'
 
 Vue.component('a-button', Button);
+Vue.component('a-icon', Icon);
 
 new Vue({
     el: '#app',
--- src/button.vue
+++ src/button.vue
@@ -1,8 +1,6 @@
 <template>
   <button class="a-button" :class="{[`icon-${iconPosition}`]: true}">
-    <svg v-if="icon" class="icon">
-      <use :xlink:href="`#i-${icon}`"></use>
-    </svg>
+    <a-icon v-if="icon" :name="icon"></a-icon>
     <div class="content">
       <slot></slot>
     </div>
