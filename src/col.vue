7371c6532e2f3807aab23098be2fd9639e0241ca
zhu <zhu_huang@126.com>
2020-08-10 06:08:06 +0800

    稍微重构了一下

--- src/col.vue
+++ src/col.vue
@@ -24,17 +24,29 @@ export default {
       gutter: 0,
     }
   },
+  methods: {
+    createClasses(obj, str = '') {
+      if (!obj) return [];
+      let arr = [];
+      if (obj.span) {
+        arr.push(`col-${str}${obj.span}`);
+      }
+      if (obj.offset) {
+        arr.push(`offset-${str}${obj.offset}`)
+      }
+      return arr;
+    }
+  },
   computed: {
     colClass() {
       let {span, offset, ipad, narrowPc, pc, widePc} = this;
-      let phoneClass= [];
+      let createClasses = this.createClasses;
       return [
-        span && `col-${span}`,
-        offset && `offset-${offset}`,
-        ... (ipad ? [`col-ipad-${ipad.span}`] : []),
-        ... (narrowPc ? [`col-narrow-pc-${narrowPc.span}`] : []),
-        ... (pc ? [`col-pc-${pc.span}`] : []),
-        ... (widePc ? [`col-wide-pc-${widePc.span}`] : []),
+        ...createClasses({span, offset}),
+        ...createClasses(ipad, 'ipad-'),
+        ...createClasses(narrowPc, 'narrow-pc-'),
+        ...createClasses(pc, 'pc-'),
+        ...createClasses(widePc, 'wide-pc-'),
       ];
     },
     colStyle() {
