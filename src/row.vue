d861ec30e4f967ad0845525eeaf60c0d8c75815f
zhu <zhu_huang@126.com>
2020-08-10 02:23:54 +0800

    重构 row 和 col

--- src/row.vue
+++ src/row.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="row" :style="{marginLeft: -gutter/2+'px', marginRight: -gutter/2+'px'}">
+  <div class="row" :style="rowStyle">
     <slot></slot>
   </div>
 </template>
@@ -11,11 +11,15 @@ export default {
       type: [Number, String]
     }
   },
-  created() {
-    console.log(this.$children);
+  computed: {
+    rowStyle() {
+      return {
+        marginLeft: -this.gutter / 2 + 'px',
+        marginRight: -this.gutter / 2 + 'px',
+      }
+    }
   },
   mounted() {
-    console.log(this.$children);
     this.$children.forEach((vm) => {
       vm.gutter = this.gutter;
     });
