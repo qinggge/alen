d135dcc7f8a18842f006058415a6ecf29b6d76d5
zhu <zhu_huang@126.com>
2020-08-12 19:04:02 +0800

    实现 position: top/bottom/middle 功能

--- src/toast.vue
+++ src/toast.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="toast" ref="wrapper">
+  <div class="toast" ref="wrapper" :class="toastClasses">
     <div class="message">
       <slot v-if="!enableHtml"></slot>
       <div v-else v-html="$slots.default[0]"></div>
@@ -34,7 +34,19 @@ export default {
     enableHtml: {
       type: Boolean,
       default: false,
+    },
+    position: {
+      type: String,
+      default: 'top',
+      validator(value) {
+       return ['top', 'bottom', 'middle'].indexOf(value) >= 0;
+      }
     }
+  },
+  computed: {
+    toastClasses() {
+      return {[`position-${this.position}`]: true}
+    },
   },
   mounted() {
     this.execAutoClose();
@@ -76,9 +88,7 @@ export default {
 
   .toast {
     position: fixed;
-    top: 0;
     left: 50%;
-    transform: translateX(-50%);
     font-size: $font-size;
     line-height: 1.8;
     min-height: $toast-min-height;
@@ -89,17 +99,29 @@ export default {
     border-radius: 4px;
     box-shadow: 0 0 3px 0 rgba(0,0,0,0.5);
     padding: 0 16px;
-  }
-  .message {
-    padding: 8px 0;
-  }
-  .close {
-    padding-left: 16px;
-    flex-shrink: 0;
-  }
-  .line {
-    height: 100%;
-    border-left: 1px solid #666;
-    margin-left: 16px;
+    .message {
+      padding: 8px 0;
+    }
+    .close {
+      padding-left: 16px;
+      flex-shrink: 0;
+    }
+    .line {
+      height: 100%;
+      border-left: 1px solid #666;
+      margin-left: 16px;
+    }
+    &.position-top {
+      top: 0;
+      transform: translateX(-50%);
+    }
+    &.position-bottom {
+      bottom: 0;
+      transform: translateX(-50%);
+    }
+    &.position-middle {
+      top: 50%;
+      transform: translate(-50%, -50%);
+    }
   }
 </style>
