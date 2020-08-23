f5dceaca9cd4660c3d45f1db067bc23a4b3255d0
zhu <zhu_huang@126.com>
2020-08-23 17:51:28 +0800

    重构

--- src/tabs.vue
+++ src/tabs.vue
@@ -30,21 +30,29 @@ export default {
       eventBus: this.eventBus,
     }
   },
-  mounted () {
-    if (this.$children.length === 0) {
-      console && console.warn &&
-      console.warn('tabs的子组件应该是tabs-head和tabs-pane，但你没有子组件');
-    }
-    this.$children.forEach((vm) => {
-      if (vm.$options.name === 'alenTabsHead') {
-        vm.$children.forEach((childVm) => {
-          if (childVm.$options.name === 'alenTabsItem'
-            && childVm.name === this.selected) {
-            this.eventBus.$emit('update:selected', this.selected, childVm);
-          }
-        })
+  methods: {
+    checkChildren() {
+      if (this.$children.length === 0) {
+        console && console.warn &&
+        console.warn('tabs的子组件应该是tabs-head和tabs-pane，但你没有子组件');
       }
-    })
+    },
+    selectTab() {
+      this.$children.forEach((vm) => {
+        if (vm.$options.name === 'alenTabsHead') {
+          vm.$children.forEach((childVm) => {
+            if (childVm.$options.name === 'alenTabsItem'
+              && childVm.name === this.selected) {
+              this.eventBus.$emit('update:selected', this.selected, childVm);
+            }
+          })
+        }
+      });
+    },
+  },
+  mounted () {
+    this.checkChildren();
+    this.selectTab();
   }
 }
 </script>
